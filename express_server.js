31a0e8f26bf70ea6265223d3995874d5095673f4
Ali Zamani <Ali.zmn@hotmail.com>
2020-12-02 21:52:21 +0000

    adding redirect and random function and post

--- express_server.js
+++ express_server.js
@@ -1,14 +1,40 @@
 const express = require('express');
 const app = express();
 const PORT = 8080 ;
+const bodyParser = require("body-parser");
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
+function generateRandomString() {
+  return Math.random().toString(36).substring(2, 8);
+}
 
-const urlDatabase = {
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.post("/urls", (req, res) => {
+  // console.log(req.body);  // Log the POST request body to the console
+  // console.log(generateRandomString());
+  let shortUrl = generateRandomString();
+  urlDatabase[shortUrl] = req.body.longURL;
+  // console.log(urlDatabase);
+  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${shortUrl}`);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  console.log(longURL);
+  console.log(urlDatabase);
+  res.redirect(longURL);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
